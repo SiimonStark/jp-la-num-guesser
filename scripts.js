3124c1630c277ed3d48197e5c085a8452f357514
Elizabeth Asbell <e.asbell8@gmail.com>
2018-12-06 11:23:57 -0700

    Slight changes

--- scripts.js
+++ scripts.js
@@ -2,3 +2,15 @@ function clearFields() {
   document.getElementById("js-clear-input1").value = "";
   document.getElementById("js-clear-input2").value = "";
 }
+
+function updateChal1(e){
+ e.preventDefault();
+ var chal1Input = document.getElementById("js-chal1").value;
+ var chal1Update = document.querySelectorAll(".js-updateChal1");
+ for (var i = 0; i < chal1Update.length; i++) {
+   chal1Update[i].innerText = chal1Input;
+ }
+}
+
+var getSubmit = document.querySelector("#js-submit");
+getSubmit.addEventListener('click', updateChal1);
