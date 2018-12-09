12a4167b6eefc8c9b7affa4e510fe4da88c9c00b
SiimonStark <justinpyktel@gmail.com>
2018-12-09 16:55:00 -0700

    validate numbers in guess

--- scripts.js
+++ scripts.js
@@ -42,23 +42,6 @@ var lowHigh2 = document.querySelector(".js-low-high2");
 
 /////////////////////////////////////////////////////////////
 
-//********************************************************
-//Justin Working on: Form Validation
-//********************************************************
-function validateRng(){
-	if (parseInt(minInput.value) > parseInt(maxInput.value)){
-			alert("Please choose a value lower than your Max!!");
-	} 
-}
-
-function validateGuess(){
-	if (parseInt(guess1.value) <= parseInt(minInput.value) || parseInt(guess1.value) >= parseInt(maxInput.value)){
-		alert("Please choose a value within the Range!!")
-	} else if (parseInt(guess2.value) <= parseInt(minInput.value) || parseInt(guess2.value) >= parseInt(maxInput.value)){
-		alert("Please choose a value within the Range!!")
-	}
-}
-//********************************************************
 
 //***************Event Listeners****************** 
 getSubmit.addEventListener('click', submitClick);
@@ -76,14 +59,14 @@ guess1.addEventListener('keyup', areFieldsEmpty);
 //***************FUNCTIONS************************
 
 //RANDOM NUM GENERATOR
-function updateRng(e){
-  e.preventDefault();
-  minUpdate.innerText = minInput.value;
-  maxUpdate.innerText = maxInput.value;
-  validateRng();
-  min = parseInt(minInput.value) || 1;
-  max = parseInt(maxInput.value) || 100;
-  numGenerated = Math.floor(Math.random() * (max - min + 1))  + min;
+function updateRng(event){
+	event.preventDefault();
+	minUpdate.innerText = minInput.value;
+	maxUpdate.innerText = maxInput.value;
+	validateRng();
+	min = parseInt(minInput.value) || 1;
+	max = parseInt(maxInput.value) || 100;
+	numGenerated = Math.floor(Math.random() * (max - min + 1))  + min;
 }
 
 
@@ -91,83 +74,126 @@ function updateRng(e){
 
 //RESET GAME (CLEAR FIELDS AND GEN NEW NUMBER)
 function resetGame(e) {
-  e.preventDefault();
-  document.getElementById("js-clear-input1").value = "";
-  document.getElementById("js-clear-input2").value = "";
-  document.getElementById("js-chal1").value = "";
-  document.getElementById("js-chal2").value = "";
-  minUpdate.innerText = minInput.value;
-  maxUpdate.innerText = maxInput.value;
-  min = parseInt(minInput.value) || 1;
-  max = parseInt(maxInput.value) || 100;
-  numGenerated = Math.floor(Math.random() * (max - min + 1))  + min;
+	e.preventDefault();
+	document.getElementById("js-clear-input1").value = "";
+	document.getElementById("js-clear-input2").value = "";
+	document.getElementById("js-chal1").value = "";
+	document.getElementById("js-chal2").value = "";
+	minUpdate.innerText = minInput.value;
+	maxUpdate.innerText = maxInput.value;
+	min = parseInt(minInput.value) || 1;
+	max = parseInt(maxInput.value) || 100;
+	numGenerated = Math.floor(Math.random() * (max - min + 1))  + min;
 } 
 
 // SUBMIT BUTTON FUNCTION
 function submitClick(e){
-  e.preventDefault();
-  updateGuesses();
-  update();
-  validateGuess();
-  player1Win();
-  player2Win();
+	e.preventDefault();
+	updateGuesses();
+	update();
+	validateGuess();
+	player1Win();
+	player2Win();
+	declareWinner();
 }
 
 //UPDATE GUESSES IN CARDS
 function updateGuesses() {
-  largeGuess1.innerText = guess1.value;
-  largeGuess2.innerText = guess2.value;
+	largeGuess1.innerText = guess1.value;
+	largeGuess2.innerText = guess2.value;
 }
 
 // UPDATE NAMES
 function update() {
-  for (var i = 0; i < chal2Update.length; i++) {
-    chal2Update[i].innerText = chal2Input.value;
-  }
-  for (var i = 0; i < chal1Update.length; i++) {
-    chal1Update[i].innerText = chal1Input.value;
-  }
+	for (var i = 0; i < chal2Update.length; i++) {
+		chal2Update[i].innerText = chal2Input.value;
+	}
+	for (var i = 0; i < chal1Update.length; i++) {
+		chal1Update[i].innerText = chal1Input.value;
+	}
 }
 
 // Functions to declare Winner
 function player1Win() {
- if (guess1.value > numGenerated){
- 	lowHigh1.innerText = "that's too high";
- } else if (guess1.value < numGenerated){
- 	lowHigh1.innerText = "that's too low";
- } else {
- 	lowHigh1.innerText = "Winner"
- }
+	if (guess1.value > numGenerated){
+		lowHigh1.innerText = "that's too high";
+	} else if (guess1.value < numGenerated){
+		lowHigh1.innerText = "that's too low";
+	} else {
+		lowHigh1.innerText = "Winner"
+	}
 }
 function player2Win(){
- if (guess2.value > numGenerated){
- 	lowHigh2.innerText = "that's too high";
- } else if (guess2.value < numGenerated){
- 	lowHigh2.innerText = "that's too low";
- } else {
- 	lowHigh2.innerText = "Winner";
- }
+	if (guess2.value > numGenerated){
+		lowHigh2.innerText = "that's too high";
+	} else if (guess2.value < numGenerated){
+		lowHigh2.innerText = "that's too low";
+	} else {
+		lowHigh2.innerText = "Winner";
+	}
 }
 
 // CLEAR GUESS INPUT
 function clearFields(e) {
-  e.preventDefault();
-  document.getElementById("js-clear-input1").value = "";
-  document.getElementById("js-clear-input2").value = "";
-  largeGuess1.innerText = "#";
-  largeGuess2.innerText = "#";
+	e.preventDefault();
+	document.getElementById("js-clear-input1").value = "";
+	document.getElementById("js-clear-input2").value = "";
+	largeGuess1.innerText = "#";
+	largeGuess2.innerText = "#";
 } 
 
 
 //ARE FIELDDS EMPTY? ONLY WORKS FOR FIRST GUESS FIELD.
 //IMPLEMENT FOR LOOP TO TARGET SECOND AS WELL?
 function areFieldsEmpty(e) {
-  e.preventDefault();
-  clearButton.disabled = false;
+	e.preventDefault();
+	clearButton.disabled = false;
 }
 
 
 
+//********************************************************
+//Justin Working on: Form Validation
+//********************************************************
+function validateRng(){
+	if (parseInt(minInput.value) > parseInt(maxInput.value)){
+		alert("Please choose a value lower than your Max!!");
+	} 
+}
+
+function validateGuess(){
+	if (parseInt(guess1.value) <= parseInt(minInput.value) || parseInt(guess1.value) >= parseInt(maxInput.value)){
+		alert("Please choose a value within the Range!!")
+	} else if (parseInt(guess2.value) <= parseInt(minInput.value) || parseInt(guess2.value) >= parseInt(maxInput.value)){
+		alert("Please choose a value within the Range!!")
+	} else if (guess1.value === "" || guess2.value === ""){
+		alert("PLEASE use only numbers!");
+	}
+}
+//===========================================
+//Next Hurdle: Print winnerName to winnerCard
+//-------------------------------------------
+function declareWinner(){
+	if (numGenerated == parseInt(guess1.value) && numGenerated == parseInt(guess2.value)) {
+		console.log('tie');
+	} else if (numGenerated == parseInt(guess1.value)) {
+		js-winner-name.innerText = chal1Input.value;
+console.log('guess1');
+	} else if (numGenerated == parseInt(guess2.value)) {
+console.log('guess2');
+	} else {
+
+	}
+
+	// if (lowHigh1.innerText === "Winner"){
+	// 	js-winner-name.innerText = chal1Input.value;
+	// } else if (lowHigh2.innerText === "Winner"){
+	// 	js-winner-name.innerText = chal2Input.value;
+	// } else if (lowHigh1.innerText === "Winner" && lowHigh2.innerText === "Winner"){
+	// 	js-winner-name.innerText = "No Ties Here Son!";
+	// }
+}
+//********************************************************
 
 
 
